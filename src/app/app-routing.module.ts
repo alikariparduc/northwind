8de26b00e1f933f45d3a3abb10774853fcd5f142
Ali Kariparduc <alikariparduc@hotmail.com>
2022-06-20 13:20:57 +0300

    200620222

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:ProductComponent},//Anasayfayı ProductComponet ayarlıyoruz.
+  {path:"products",component:ProductComponent}// /'den sonra products gelirse yine productcomponent getir.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
